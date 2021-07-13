158a8dd1079c58b2975cd303037567d4c9532a7c
Maulik Kanani <technocredits.oct2020@gmail.com>
2021-07-13 08:34:19 +0530

    Added Branch B1 changes

--- src/package2/Class2.java
+++ src/package2/Class2.java
@@ -4,7 +4,8 @@ public class Class2 {
 	
 	public static void main(String[] args) {
 		System.out.println("2");
-		System.out.println("2.1");
+		System.out.println("2.1 In Branch B");
+		System.out.println("Checking conflicts scenario");
 	}
 
 }
