5516bf8b526de34b24a0590338d920d7130681a5
Maulik Kanani <technocredits.oct2020@gmail.com>
2021-07-13 23:53:14 +0530

    Second COmmit

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -7,5 +7,6 @@ public class Class1 {
 		System.out.println("1.1 Branch A");
 		System.out.println("Hey, added enw line in Branch B");
 		System.out.println("First Commit");
+		System.out.println("Second Commit");
 	}
 }
